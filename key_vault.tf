3c4ffcec22786f83187442f406a5bae0c9ae60cb
ydvsailendar <ydvsailendar@gmail.com>
2025-09-14 18:36:22 +0100

    vnet subnet and log analytics

--- key_vault.tf
+++ key_vault.tf
@@ -11,6 +11,6 @@ resource "azurerm_key_vault" "kv" {
   access_policy {
     tenant_id          = data.azurerm_client_config.current.tenant_id
     object_id          = data.azurerm_client_config.current.object_id
-    secret_permissions = ["get", "list", "set", "delete"]
+    secret_permissions = ["Get", "List", "Set", "Delete"]
   }
 }
